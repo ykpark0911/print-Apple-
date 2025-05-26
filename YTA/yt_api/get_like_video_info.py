4c4668a6aace0bd5be66ea2b6bd5c018a9608308
박연규 <121869703+ykpark0911@users.noreply.github.com>
2025-05-26 16:53:15 +0900

    Add files via upload

---
+++ YTA/yt_api/get_like_video_info.py
@@ -0,0 +1,66 @@
+# ── Google API 호출을 좋아요한 재생목록 영상과 그 정보를 불러오는 모듈 ──
+
+from tool import is_short, duration_iso8601_to_seconds, get_categoryId
+
+# 좋아요 한 영상의 정보를 불러오는 함수
+# youtube: 유튜브 객체
+def extract_video_info_from_like_playlist(youtube):
+    # 호출한 좋아요 영상 정보를 담는 리스트
+    like_video_info_list = []
+
+    # 구글에 보낼 영상 정보 요청 사항
+    # 재생목록 조회를 위한 API 요청 구성
+    # videos(): 영상 관련 API 요청들을 포함한 리소스 생성
+    # list(): API 요청 정보를 담고 있는 객체 생성
+    # part: 파라미터 (어떤 정보만 받을지)
+    # myRating: 
+    # maxResults: 
+    request = youtube.videos().list(
+        part="snippet, contentDetails",
+        myRating="like", #좋아요한 영상 재생목록
+        maxResults=50 #최대 50개
+    )
+
+    # 
+    while request:
+
+        # 요청 받은 좋아요한 영상 정보
+        response = request.execute()
+        # item: 응답 받은 영상의 정보(dict)를 꺼냄 예) {"kind": "youtube#video", "id": "abc123", "snippet": {"title": "파이썬 강의","channelTitle": "코딩하는 정대리““category”: "Music"}}
+        for item in response["items"]:
+
+            # video_info_list에 추가할 영상 정보
+            like_video_info = {
+                "id": item["id"],
+                "title": item["snippet"]["title"],
+                "category": get_categoryId(item["snippet"].get("categoryId", "0")),
+                "channel": item["snippet"]["channelTitle"],
+                "thumbnails" : item['snippet']['thumbnails']['high'],
+                "durationSec": duration_iso8601_to_seconds(item["contentDetails"]["duration"]),
+                "isShort": is_short(item),
+            }
+            #like_video_info_list에 추가
+            like_video_info_list.append(like_video_info)
+
+        # 다음 영상 정보 요청 사항 생성
+        # list_next: 자동으로 다음 페이지를 요청할 수 있는 request 객체를 반환하는 함수
+        request = youtube.videos().list_next(request, response) 
+
+    return like_video_info_list
+'''
+[
+    {'id': 'fTwZNlF9K8E',
+    'title': '閻羅 (cover)\u200b\u200b\u200b\u200b\u200b / \u200bヒュボク',
+    'category': '10',
+    'channel': '휴복',
+    'thumbnails': {'url': 'https://i.ytimg.com/vi/fTwZNlF9K8E/hqdefault.jpg',
+    'width': 480, 'height': 360}, 'durationSec': 273.0, 'isShort': 'notShort',
+    'dateTimes': [datetime.datetime(2025, 5, 19, 0, 19, 31, 433000, tzinfo=tzutc())]
+    },
+    {
+    .
+    .
+    .
+    }
+]
+'''
---
+++ YTA/yt_api/oauth.py
@@ -0,0 +1,21 @@
+# ── Google OAuth 2.0 인증을 통해 YouTube API 사용 권한을 얻기 위한 모듈 ──
+
+from google_auth_oauthlib.flow import InstalledAppFlow  # 사용자 OAuth 2.0 인증 처리 클래스
+from googleapiclient.discovery import build             # API 요청을 위한 서비스 객체 생성 함수
+
+# SCOPES = 사용자의 YouTube 데이터 접근 권한 요청 범위
+SCOPES = ["https://www.googleapis.com/auth/youtube.readonly"] # 읽기 전용 권한
+
+def get_authenticated_service():
+    # OAuth 흐름 초기화 (client_secret.json 파일과 요청 권한 설정)
+    flow = InstalledAppFlow.from_client_secrets_file(
+        "C:\\pypy\\YTA-file\\YTA\\yt_api\\client_secret_693447563027-1qjb80j8h2uj0phs3cvqfjcnb8tsat4m.apps.googleusercontent.com.json", SCOPES
+    )
+
+    # 사용자 브라우저 열기 → 로그인 + 권한 허용 → 인증 정보 받기
+    credentials = flow.run_local_server(port=0)
+    
+    # 인증된 credentials로 YouTube API 요청 메서드들 갖은 객체 생성
+    youtube = build("youtube", "v3", credentials=credentials)
+
+    return youtube  # 인증된 YouTube 객체 반환
